a3b73339ebd5b99f43e6e54c579c8fe5e2adb569
sofish <happinesz@gmail.com>
2012-07-29 20:33:12 +0800

    reset indent with 4 spaces

--- jquery-storage.js
+++ jquery-storage.js
@@ -5,60 +5,60 @@
     // document.domain = 'example.com';
     // and please uncomment and set it to your own as well as on storage.html
 
-	if(typeof window.localStorage == 'undefined'){
-		var iframe = $('<iframe style="width:0;height:0;" frameBorder="0"></iframe>'),
-			i = 0,
-			ret = {};
+    if(typeof window.localStorage == 'undefined'){
+        var iframe = $('<iframe style="width:0;height:0;" frameBorder="0"></iframe>'),
+            i = 0,
+            ret = {};
 
-		iframe.on('load', function(e){
-			var context = this.contentWindow.localStorage;
+        iframe.on('load', function(e){
+            var context = this.contentWindow.localStorage;
 
-			ret['get'] = context.getItem;
-			ret['set'] = context.setItem;
-			ret['clear'] = context.clear;
-		});
+            ret['get'] = context.getItem;
+            ret['set'] = context.setItem;
+            ret['clear'] = context.clear;
+        });
 
-		// TODO: remember to set a right path to `iframe.php` 
-		iframe.attr('src', './storage.php');
-		$('body').append(iframe);
-	}
+        // TODO: remember to set a right path to `iframe.php`
+        iframe.attr('src', './storage.php');
+        $('body').append(iframe);
+    }
 
-	function machine(){
+    function machine(){
 
-		if(typeof window.localStorage == 'undefined'){
+        if(typeof window.localStorage == 'undefined'){
 
-			return {
-				get: function(key){
-					return ret.get ? ret.get(key) : setTimeout(arguments.callee, 100)
-				},
-				set: function(key, value){
-					return ret.set ? ret.set(key, value) : setTimeout(arguments.callee, 100)
-				},
-				remove: function(key, value){
-					return ret.set ? ret.remove(key) : setTimeout(arguments.callee, 100)
-				},
-				clear: function(){
-					return ret.clear ? ret.clear() : setTimeout(arguments.callee, 100)
-				}
-			}
-		}
+            return {
+                get: function(key){
+                    return ret.get ? ret.get(key) : setTimeout(arguments.callee, 100)
+                },
+                set: function(key, value){
+                    return ret.set ? ret.set(key, value) : setTimeout(arguments.callee, 100)
+                },
+                remove: function(key, value){
+                    return ret.set ? ret.remove(key) : setTimeout(arguments.callee, 100)
+                },
+                clear: function(){
+                    return ret.clear ? ret.clear() : setTimeout(arguments.callee, 100)
+                }
+            }
+        }
 
-		return {
-			set: function(key, value){
-				return localStorage.setItem(key, value)
-			},
-			get: function(key){
-				return localStorage.getItem(key)
-			},
-			remove: function(key){
-				return localStorage.removeItem(key)
-			},
-			clear: function(){
-				return localStorage.clear()
-			}
-		}	
-	}
+        return {
+            set: function(key, value){
+                return localStorage.setItem(key, value)
+            },
+            get: function(key){
+                return localStorage.getItem(key)
+            },
+            remove: function(key){
+                return localStorage.removeItem(key)
+            },
+            clear: function(){
+                return localStorage.clear()
+            }
+        }
+    }
 
-	$.fn.storage = machine();
+    $.fn.storage = machine();
 
 })(jQuery)
